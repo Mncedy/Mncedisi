c8e4877c77abda7f511bb228bf3bc229d5ca105c
Mncedy <Mncedimc@gmail.com>
2020-08-31 05:58:50 +0200

    Updates

---
+++ precache-manifest.4ba435432931423de1be0c3577ee4bed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab1bf2f0768452e5f5b47c6d71b77e1",
+    "url": "/Mncedy/Mncedisi.git/index.html"
+  },
+  {
+    "revision": "5ad6fe16651f7b830bb2",
+    "url": "/Mncedy/Mncedisi.git/static/css/main.c5a34066.chunk.css"
+  },
+  {
+    "revision": "80c401acda344cacfbd9",
+    "url": "/Mncedy/Mncedisi.git/static/js/2.1b5dbc6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Mncedy/Mncedisi.git/static/js/2.1b5dbc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad6fe16651f7b830bb2",
+    "url": "/Mncedy/Mncedisi.git/static/js/main.09d1d99f.chunk.js"
+  },
+  {
+    "revision": "aaa1a67aa0c73a1d8269",
+    "url": "/Mncedy/Mncedisi.git/static/js/runtime-main.22a8e287.js"
+  },
+  {
+    "revision": "719fadc6fb982a3d99d2c1ccb9057728",
+    "url": "/Mncedy/Mncedisi.git/static/media/IMG_202004-011.719fadc6.jpg"
+  },
+  {
+    "revision": "98946e7cde33a00a6652691e06a40380",
+    "url": "/Mncedy/Mncedisi.git/static/media/drakensberg.98946e7c.jpg"
+  },
+  {
+    "revision": "4b8e3eb28839f3e5ab2711bbe130fbc4",
+    "url": "/Mncedy/Mncedisi.git/static/media/logo.4b8e3eb2.svg"
+  }
+]);
