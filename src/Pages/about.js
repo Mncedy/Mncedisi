5375101effbe3ad786dadbafba6929e3e1a38008
Mncedy <Mncedimc@gmail.com>
2020-09-03 04:35:06 +0200

    Updating contact page

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -1,6 +1,5 @@
 import React from "react";
 import IMG from "../images/IMG_202004-011.jpg";
-import Skills from "../assets/skills";
 
 export const About = () => {
   return (
@@ -12,27 +11,56 @@ export const About = () => {
           <h3>Junior Business Analyst</h3>
           <div></div>
           <p id="pTag1">
-            Solution-driven Business Analyst, no experience yet.
-            <br /> Collaborative, can work with cross-functional teams in the
-            development, documentation and delivery of process innovations
-            driving the accomplishments of business goals,
-            <br /> Seek opportunitties to transform company practise into fresh,
-            cost-effective solutions leading to more efficient operations!
+            <strong>
+              Solution-driven Business Analyst, no experience yet.
+              <br /> Collaborative, can work with cross-functional teams in the
+              development, documentation and delivery of process innovations
+              driving the accomplishments of business goals,
+              <br /> Seek opportunitties to transform company practise into
+              fresh, cost-effective solutions leading to more efficient
+              operations!
+            </strong>
           </p>
           <h3>Toolkit</h3>
           <p id="pTag1">
-            <ul>• Microsoft Office</ul>
-            <ul>• Microsoft Visio</ul>
-            <ul>• Microsoft Powerpoint</ul>
-            <ul>• Microsoft Power BI</ul>
-            <ul>• Google Docs</ul>
-            <ul>• Trello</ul>
+            <strong>
+              <ul>• Microsoft Office</ul>
+              <ul>• Microsoft Visio</ul>
+              <ul>• Microsoft Powerpoint</ul>
+              <ul>• Microsoft Power BI</ul>
+              <ul>• Google Docs</ul>
+              <ul>• Trello</ul>
+            </strong>
           </p>
           <div></div>
+          <h3>Course key areas</h3>
+          <div>
+            <p id="pSkill">
+              <strong>
+                • Gathering Requirements (Project Charter, Business Requirements
+                Document),
+                <br /> • Information Management (Microsoft SQL),
+                <br /> • Business Information System Analysis,
+                <br /> • Strategy analysis - Defining the BA stakeholder
+                collaboration tasks to identify strategic need and importance of
+                the change and align this with greater organizational
+                strategies.,
+                <br /> • Elicitation and collaboration - Defining the tasks to
+                obtain information and communication with stakeholders,
+                <br /> • Underlying Competencies,
+                <br /> • SAP Enterprise Resource Planning Software System
+                (Procurement, Material Planning, Sales Order Management).
+                <br /> • ASP.NET MVC
+              </strong>
+            </p>
+          </div>
         </div>
         <div className="edu">
           <h2 id="h2Tag">Education</h2>
-          <span>2017-2019 • Durban University of Technology</span>
+          <span>
+            February 2017- November 2019
+            <br /> • Durban University of Technology
+          </span>
           <h4>
             Information and Communication Technology Diploma in Business
             Analysis
@@ -43,7 +71,10 @@ export const About = () => {
             facilitate <br /> the alignment of IT and business processes.
           </p>
           <hr></hr>
-          <span>2011-2012 • Mnambithi FET College</span>
+          <span>
+            February 2011- June 2012
+            <br /> • Mnambithi FET College
+          </span>
           <h4>N6 Diploma in Financial Management</h4>
           <p id="pTag">
             Learners will demonstrate the ability to select and apply financial
@@ -56,15 +87,18 @@ export const About = () => {
           </p>
           <hr></hr>
           <h2 id="h2Tag">Working Experience</h2>
-          <span>NOVEMBER 2012 – JANUARY 2017 • Khethekile Mining</span>
+          <span>
+            NOVEMBER 2012 – JANUARY 2017
+            <br /> • Khethekile Mining
+          </span>
           <p id="pTag">
             Designation: Electric Drill Operator • (Safety Representative)
             <br />
-            Reference: Jessica Leroux (+27) 82 658 5419
+            Reference: Jessica Leroux
+            <br /> Phone: (+27) 82 658 5419
           </p>
         </div>
       </div>
-      <Skills />
     </>
   );
 };
--- src/Pages/contact.js
+++ src/Pages/contact.js
@@ -9,16 +9,70 @@ const Contact = () => {
       <div className="contact-div">
         <h2>Mncedisi Makhaza</h2>
         <img className="pic" src={IMG} alt="Myself" />
-        <p>Born and raised in the valley of Bergville.</p>
+        <h3>WHERE I'VE BEEN</h3>
+        <p id="contactP">
+          Born and raised in the valley of Bergville. And the following is
+          cliché but true.
+          <br /> I've plough with oxen, harvest the fields, along with all sorts
+          of labor intensive jobs, you name it, even worked in the restaurant
+          business.
+          <br /> After completing matric, I worked in a restuarant Tower of
+          Pizza which is nearby my village for a year.
+          <br />
+          And then after I went to Mncambithi FET college, where I obtained my
+          N4, N5 and N6 certification.
+          <br /> Few months later I was an employee at Khethekile Mining, where
+          i worked as an Electric Drill Operator for 4 years. I was elected as a
+          safety representative for my team.
+          <br /> All this has given me a new and unique perspective. Whether in
+          personal matter or in projects, the same values hold true.
+          <br />
+          <strong>
+            {" "}
+            What is the problem? And what is the solution/s for that problem.
+          </strong>
+        </p>
+        <h3>WHAT I'M DOING</h3>
+        <p id="contactP">Personal development</p>
+        <h3>WHAT I HOPE TO DO</h3>
+        <p id="contactP">
+          Change the world like Elon musk.., or Disrupt Forbes list like Jeff
+          Bezos..
+          <br />
+          Seriously though... I hope to be the be the best Analyst & husband I
+          can possibly be. Everyday is a chance at personal
+          improvement/development.
+          <br />
+          So my plan is to get better, do better. Personally and professionally.
+          I am where I am because of my decisions, if I want better results I
+          must take better actions.
+          <br />
+          <strong>
+            I've learned the lessons, I will not stop until my goals are
+            realize. When I reach that goal I will set a bigger goal. I will
+            grow and grow and grow. I accept full responsibility for my results
+            in life, and I challenge myself to become better.
+            <br />
+            Every year, Every month, Everyday, Every moment. I am commited, I am
+            determined, I AM SELF-MADE!
+          </strong>
+        </p>
       </div>
       <div className="contact-div">
         <h2>Contact Me</h2>
         <FaPhoneAlt className="icons" />
-        <p> (+27) 76 245 2771</p>
-        <MdEmail className="icons" /> <p>Mncedimc@gmail.com</p>
+        <p>
+          <strong> (+27) 76 245 2771</strong>
+        </p>
+        <MdEmail className="icons" />{" "}
+        <p>
+          <strong>Mncedimc@gmail.com</strong>
+        </p>
         <FaAddressBook className="icons" />
         <p>
-          212 Sphiwe Zuma Avenue <br /> Glenmore <br /> Durban <br /> 4001
+          <strong>
+            212 Sphiwe Zuma Avenue <br /> Glenmore <br /> Durban <br /> 4001
+          </strong>
         </p>
       </div>
     </div>
